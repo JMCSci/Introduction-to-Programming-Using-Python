be000925d07faebcc153f24cfa28addf16d04396
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-06-03 13:13:03 -0400

    Add Area.py

---
+++ Chapter 1/1.8/Area.py
@@ -0,0 +1,16 @@
+'''
+Chapter 1.8
+
+Created on Jun 3, 2020
+
+@author: jasonmoreau
+'''
+
+PI = 3.14
+RADIUS = 5
+
+area = RADIUS * RADIUS * PI
+perimeter = 2 * RADIUS * PI
+
+print(area)
+print(perimeter)
