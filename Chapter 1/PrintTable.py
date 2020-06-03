560bd5aeedb21c1e634e0b5985700e13b83f1336
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-06-02 20:12:46 -0400

    Create PrintTable.py

---
+++ Chapter 1/PrintTable.py
@@ -0,0 +1,13 @@
+'''
+Chapter 1.4
+
+Created on Jun 2, 2020
+
+@author: jasonmoreau
+'''
+
+print("a        a^2        a^3")
+print("1        1          1")
+print("2        4          8")
+print("3        9          27")
+print("4        16         64")
