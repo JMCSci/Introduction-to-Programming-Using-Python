a4793431510c3d24d1870252de25db2137756250
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-06-02 20:12:09 -0400

    Create DisplayPattern.py

---
+++ Chapter 1/1.3/DisplayPattern.py
@@ -0,0 +1,13 @@
+'''
+Chapter 1.3
+
+Created on Jun 2, 2020
+
+@author: jasonmoreau
+'''
+
+print("FFFFFFF   U     U   NN     NN")
+print("FF        U     U   NNN    NN")
+print("FFFFFFF   U     U   NN N   NN")
+print("FF         U   U    NN  N  NN")
+print("FF          UUU     NN    NNN") 
