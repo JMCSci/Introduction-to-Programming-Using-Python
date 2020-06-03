c7ffcab2084060dafa91dbefc024d5cb52b35ee6
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-06-03 13:14:49 -0400

    Add AverageSpeed.py

---
+++ Chapter 1/1.10/AverageSpeed.py
@@ -0,0 +1,16 @@
+'''
+Chapter 1.10
+
+Created on Jun 3, 2020
+
+@author: jasonmoreau
+'''
+
+# 1 mile = 1.6 kilometers
+# 60 minutes = 1 hour
+# 14 minutes and 30 seconds left
+
+miles_ran = 14 / 1.6            # 8.75 miles
+time_ran = 45.5 / 60            # 0.758333333333
+average_speed = miles_ran / time_ran
+print(average_speed)            # 11.5384615385
