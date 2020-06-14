f2b0bf2f36e339de2c0502cc1f978a326fb6fac3
Jason Moreau <suprstar@outlook.com>
2020-06-13 21:10:05 -0400

    Add my solutions to Chapter 8

--- Chapter 8/8.18/circle2d/Circle2D.py
+++ Chapter 8/8.18/circle2d/Circle2D.py
@@ -8,27 +8,22 @@ class Circle2D:
         self.__y = y
         self.__radius = radius
         
-    
     def getPerimeter(self):
         perimeter = 2 * math.pi * self.__radius
         return perimeter
-    
     
     def getArea(self):
         area = math.pi * math.pow(self.__radius, 2)
         return area
     
     def containsPoint(self, x, y):
-        # distance between the two points is less than the radius
         distance = math.sqrt(math.pow(x - self.__x,2) + math.pow(y - self.__y, 2))
         if(distance < self.__radius):
             return True
         return False
     
     def contains(self, Circle2D):
-        # if distance of points is less than than self.radius
         distance = math.sqrt(math.pow(Circle2D.getX() - self.__x,2) + math.pow(Circle2D.getY() - self.__y, 2))
-        # if radius is less self.radius
         if(Circle2D.getPerimeter() < self.getPerimeter()):
             if(distance < self.__radius):
                 return True
@@ -41,6 +36,35 @@ class Circle2D:
             return True
         return False
     
+    def __contains__(self, another):
+        return self.contains(another)
+    
+    def __cmp__(self, another):
+        if(self.__radius > another.__radius):
+            return 1
+        elif(self.__radius == another.__radius):
+            return 0
+        else:
+            return -1
+        
+    def __lt__(self, another):
+        return self.__cmp__(another) < 0
+    
+    def __le__(self, another):
+        return self.__cmp__(another) <= 0
+    
+    def __eq__(self, another):
+        return self.__cmp__(another) == 0
+    
+    def __gt__(self, another):
+        return self.__cmp__(another) > 0
+    
+    def __ge__(self, another):
+        return self.__cmp__(another) >= 0
+    
+    def __ne__(self, another):
+        return self.__cmp__(another) != 0  
+    
     def getX(self):
         return self.__x
     
@@ -49,7 +73,6 @@ class Circle2D:
     
     def getRadius(self):
         return self.__radius
-    
     
     def setX(self, value):
         self.__x = value
