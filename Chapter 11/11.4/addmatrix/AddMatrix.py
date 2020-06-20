db83e7ef92367663b6aa6e83903add1910154d52
Jason Moreau <suprstar@outlook.com>
2020-06-19 20:30:07 -0400

    Add solution to Chapter 11

--- Chapter 11/11.4/addmatrix/AddMatrix.py
+++ Chapter 11/11.4/addmatrix/AddMatrix.py
@@ -1,9 +1,12 @@
-''' Chapter 11.5 '''
+'''' Chapter 11.5 '''
 
 def main():
-    createMatrix()
+    a, b = createMatrix()    
+    c = addMatrix(a, b)
+    printMatrix(c)
    
-   
+# 1 2 3 4 5 6 7 8 9
+# 0 2 4 1 4.5 2.2 1.1 4.3 5.2
    
 def createMatrix():
     numberOfRows = eval(input("Enter the number of rows: "))
@@ -11,46 +14,43 @@ def createMatrix():
     a = []
     b = []
     
-    print(numberOfRows,"X",numberOfColumns,"matrix created")
+    print("Two",numberOfRows,"X",numberOfColumns,"matrices created.")
     
-    print("\nEnter element values for Matrix A")
-    for row in range(numberOfRows):
-        a.append([])    # add row
-        for column in range(numberOfColumns):
-            value = eval(input("Enter an element: "))
-            a[row].append(value) # add element to row index
-      
-    print("\nEnter element values for Matrix B")      
-    for row in range(numberOfRows):
-        b.append([])
-        for column in range(numberOfColumns):
-            value = eval(input("Enter an element: "))
-            b[row].append(value)
-    
-    return a,b
+    numbers1 = input("\nEnter numbers for Matrix A: ") # add element to row index
+    elements1 = numbers1.split()
+    for i in range(numberOfRows):
+        list1 = [eval(elements1[x]) for x in range(numberOfRows * i, numberOfColumns * i + numberOfRows)]
+        a.append(list1)
+            
+    numbers2 = input("\nEnter numbers for Matrix B: ")
+    elements2 = numbers2.split()
+    for i in range(numberOfRows):
+        list2 = [eval(elements2[x]) for x in range(numberOfRows * i, numberOfColumns * i + numberOfRows)]
+        b.append(list2)
+            
+    return a, b
 
-def printMatrix(a, b):
-    print("Matrix A")
-    for i in a:
-        for j in i:
-            print(j, end = " ")
+def addMatrix(a, b):
+    rows = len(a)
+    columns = len(a[0])
+    c = []
+    
+    for i in range(rows):
+        c.append([])
+        for j in range(columns):
+            sum = 0
+            sum += a[i][j] + b[i][j]
+            c[i].append(sum)
+    
+    return c
+
+def printMatrix(matrix):
+    for row in matrix:
+        for value in row:
+            print(format(value, ".1f"), end = format(" ", ">2s"))
         print()
     
     print()
     
-    print("Matrix B")
-    for i in b:
-        for j in i:
-            print(j, end = " ")
-        print()
-    
-    
-    return a, b  
-    
-
-def addMatrix(a, b):
-    pass
-
-
 if __name__ == "__main__":
     main()
